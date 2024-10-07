84c0108ff1417cded982c9c645800869e82bb5f5
Damian <dg997678@gmail.com>
2024-10-07 14:38:59 -0500

    feat: :sparkles: FEAT
    Add
    
    adding configuration of main, app module, plus the commons, filter, decorators, exceptions folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlayerModule } from './player/player.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 5432), 
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, 
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+    PlayerModule,
+    AuthModule,
+    
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './common/excepcion-filters/exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter)
+
+  app.useGlobalInterceptors(new ResponseInterceptor)
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Final Test Docs')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
